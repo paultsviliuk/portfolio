641c209466be34374ddbeb702d29c1d1338752b7
Paul Tsviliuk <paultsvilyuk@gmail.com>
2023-04-22 21:08:22 -0700

    Added presentation section.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,48 @@
-import React from 'react'
+import { motion } from 'framer-motion';
+
+import { styles } from '../styles';
+import { ComputersCanvas } from './canvas';
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className='relative w-full h-screen
+    mx-auto'>
+      <div className={`${styles.paddingX} absolute
+      insert-0 top-[120px] max-w-7xl mx-auto flex
+      flex-row items-start gap-5`}>
+        <div className='flex flex-col
+        justify-center items-center mt-5'>
+          <div className='w-5 h-5 rounded-full 
+          bg-[#915eff]' />
+          <div className='w-1 sm:h-80 h-40
+          violet-gradient' />
+        </div>
+
+        <div>
+          <h1 className={`${styles.heroHeadText}
+          text-white`}>Hi, I'm <span 
+          className="text-[#915eff]">Paul
+          </span></h1>
+          <p className={`${styles.heroSubText} mt-2
+          text-white-100`}>
+            Experienced Software Engineer <br 
+            className="sm:block hidden" />
+            seeking new opportunities to <br 
+            className="sm:block hidden" />
+            create innovative solutions. <br 
+            className="sm:block hidden" />
+            Skilled in microservice design, <br 
+            className="sm:block hidden" />
+            development, and integration, <br 
+            className="sm:block hidden" />
+            optimizing existing platforms <br 
+            className="sm:block hidden" />
+            for increased efficiency.
+          </p>
+        </div>
+      </div>
+
+    </section>
   )
 }
 
